3bbdb08df532d92eaf291dbe3fd45087ff50afdc
HU <HU@192.168.0.56>
2020-10-21 11:06:37 +0900

    test

---
+++ src/work/new111.java
@@ -0,0 +1,10 @@
+package work;
+
+public class new111 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/work2/asas.java
@@ -0,0 +1,10 @@
+package work2;
+
+public class asas {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
